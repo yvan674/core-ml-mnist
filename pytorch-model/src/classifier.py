363aa9d7c8f84ffda5c4b1f5fd195189c59a9155
Yvan Satyawan <y_satyawan@hotmail.com>
2019-04-01 16:26:03 +0200

    Working classifier network

--- pytorch-model/src/classifier.py
+++ pytorch-model/src/classifier.py
@@ -26,17 +26,30 @@ class Classifier(nn.Module):
         self.conv2 = nn.Sequential(
             nn.Conv2d(10, 20, 5),
             nn.Dropout2d(),
-            # nn.MaxPool2d(2),
+            nn.MaxPool2d(2),
             nn.ReLU()
         )
-        self.fc1 = nn.Linear(320, 50)
+        self.fc1 = nn.Linear(2000, 50)
         self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
-        x = x.view(-1, 320)
+        x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+
+    @staticmethod
+    def num_flat_features(x):
+        """Multiplies the number of features for flattening a convolution.
+        References:
+            https://pytorch.org/tutorials/beginner/blitz/neural_networks_
+            tutorial.html
+        """
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
