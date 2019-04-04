9c5d227cdc2a9c9ce5ea722d6ddfd9095ff7ac9a
Yvan Satyawan <y_satyawan@hotmail.com>
2019-04-04 07:06:29 +0200

    Initial commit with app

---
+++ MNIST Test App/MNIST Test App/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  MNIST Test App
+//
+//  Created by Yvan Putra Satyawan on 03/04/19.
+//  Copyright © 2019 yvan. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
